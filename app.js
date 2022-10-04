34593c56e16d4d516935bf967660df42beec01a0
Cleophas <cleophasfoy@gmail.com>
2022-10-03 20:08:36 -0500

    added jumping logic and style for obstacles

--- app.js
+++ app.js
@@ -1,27 +1,37 @@
 document.addEventListener('DOMContentLoaded', () => {
   const dino = document.querySelector('.dino')
+  const grid = document.querySelector('.grid')
   let isJumping = false
+  let gravity = 0.9
 
   function control(e) {
     if (e.keyCode === 32) {
-      jump()
+      if (!isJumping) {
+        isJumping = true
+        jump()
+      }
     }
   }
   document.addEventListener('keyup', control)
 
+  let position = 0
   function jump() {
+    let count = 0
     let position = 0
     let timerId = setInterval(function () {
 
       //move down
-      if (position === 150) {
+      if (count === 15) {
         clearInterval(timerId)
         console.log('down')
         let downTimerId = setInterval(function () {
-          if (position === 0) {
+          if (count === 0) {
             clearInterval(downTimerId)
+            isJumping = false
           }
-          position -= 30
+          position -= 5
+          count --
+          position = position * gravity
           dino.style.bottom = position + 'px'
         }, 20)
       }
@@ -29,13 +39,24 @@ document.addEventListener('DOMContentLoaded', () => {
 
       console.log('up')
       position += 30
+      count++
+      position = position * gravity
       dino.style.bottom = position + 'px'
     },20)
   }
 
+  function generateObstacles() {
+    let obstaclePosition = 1000
+    const obstacle = document.createElement('div')
+    obstacle.classList.add('obstacle')
+    grid.appendChild(obstacle)
+    obstacle.style.left = obstaclePosition + 'px'
+  }
 
+  generateObstacles()
 
 
 
 
 })
+//LEFT off at 16:15  https://www.youtube.com/watch?v=dQ6lYd6dyTI
